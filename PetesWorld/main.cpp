c60ea628b1e9ffaeeabcf1085e53124efb0d8d5a
(Student  C00314875) Zaid Mb Elkhazendar <C00314875@setu.ie>
2024-11-08 12:47:49 +0000

    D letter by Zaid

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -22,7 +22,7 @@ int main()
 		std::cout << "You need to select a number between 1 and 5" << std::endl;
 		return 0;
 	}
-	std::cout << "Pick a letter from A to C or W to Z" << std::endl; // change this line each iteration
+	std::cout << "Pick a letter from A to D or W to Z" << std::endl; // change this line each iteration
 	std::cin >> letter;
 	if (letter < 'A' || letter > 'Z') // not a capital letter
 	{
@@ -102,6 +102,29 @@ int main()
 			std::cout << "Cocos (Keeling) Islands, Cook Islands" << std::endl;
 		}
 	}	
+	if (letter == 'd' || letter == 'D')//written by Zaid
+	{
+		if (region == 1)
+		{
+			std::cout << "Dominica, Dominican Republic" << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "Denmark" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Djibouti"<< std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no countries in this region." << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are no countries in this region." << std::endl;
+		}
+	}
 	if (letter == 'w' || letter == 'W') // written by Marie-Elise
 	{
 		if (region == 1)
